633f28d6f589767b98b26be6f2b25bd02702a338
onchari <onchariwycliffe@gmail.com>
2018-05-17 20:59:16 +0300

    Add project files.

---
+++ MyElectronix/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyElectronix.Startup))]
+namespace MyElectronix
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
